2752c6f5697fb2ff55b0e8e7ed8e6ca93740bef7
nms553 <44218966+nms553@users.noreply.github.com>
2022-04-15 21:47:31 -0500

    Update library.js

--- library.js
+++ library.js
@@ -1,12 +1,33 @@
+Array.prototype.vlookup = vlookup
 
-
-
-function vlookup(searchvalue, array, headervalue, overwrite_value) {
-  
-  var foreach = array.forEach(function(item, index) {
-    if (item[0] == searchvalue) {
-      return item[index]
+function vlookup(searchvalue, headervalue, overwrite_value, lookupindex) {
+  var foreach
+  var that = this
+  var startindex = 0
+  if (lookupindex) {
+    if (isNaN(lookupindex)) {
+      startindex = that[0].indexOf(lookupindex)
+    } else {
+      startindex = lookupindex
     }
   }
+  if (isNaN(headervalue)) {
+    headervalue = that[startindex].indexOf(headervalue)
+  }
+
+  this.forEach(function(item, index) {
+    if (item[startindex] == searchvalue) {
+      if (overwrite_value) {
+        item[headervalue] = overwrite_value
+        foreach = that
+      } else {
+        foreach = item[headervalue]
+      }
+
+    }
+  })
+  console.log(foreach)
   return foreach;
+
+
 }

dc675c5f156a72bc9461a7bc9a2653106e88e5d1
nms553 <44218966+nms553@users.noreply.github.com>
2022-10-05 14:20:13 -0500

    Update and rename xml.html to xml.js

---
+++ xml.js
@@ -0,0 +1,20 @@
+//XMLHHTP GRAB FUNCTION -- DO NOT EDIT ANYTHING BELOW
+
+var 
+
+function loadXMLDoc(url, x) {
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+
+      var json = JSON.parse(this.responseText)
+      window[x](json[0])
+      console.log(json[0])
+    }
+  };
+  xhttp.open("GET", encodeURI(url), true);
+  xhttp.send();
+  
+}
+
